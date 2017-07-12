dcd3d5dfa3b78b5bb7b27fb9d6ebc5fb50ab358e
Haoz <developerhaon@gmail.com>
2017-07-12 19:48:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/developerhaoz/doodleview/DoodleView.java
@@ -0,0 +1,10 @@
+package com.example.developerhaoz.doodleview;
+
+import android.view.SurfaceView;
+
+/**
+ * Created by developerHaoz on 2017/7/12.
+ */
+
+public class DoodleView extends SurfaceView {
+}
